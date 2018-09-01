deb31c217e5e4c3341e2d8518060d9a9b197a661
shshen <iamshensh@outlook.com>
2018-09-01 22:13:44 +0800

    feat : add MongoDB cli

---
+++ src/grid-test.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://127.0.0.1:27017/";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("griddb");
+    var whereStr = {"buyDate": "2018-5-15"}; 
+    let emptyStr = {};
+    dbo.collection("grid"). find(emptyStr).toArray(function(err, result) { // 返回集合中所有数据
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    });
+});
