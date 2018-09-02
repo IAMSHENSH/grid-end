3d417760edca6102579901d36d847ae2cdd05d71
shshen <iamshensh@outlook.com>
2018-09-02 21:26:28 +0800

    feat : add db api - 2

--- src/grid-db.ts
+++ src/grid-db.ts
@@ -12,19 +12,19 @@ const dbName = 'griddb';
  * sellDate : string; 卖出时间
  */
 interface Grid {
-	type : number;
-	amount : number;
-	buyPrice : number;
-	buyDate : Date;
+	type: number;
+	amount: number;
+	buyPrice: number;
+	buyDate: Date;
 	sellPrice ? : number;
 	sellDate ? : Date;
 }
 
 // 网格规则 - 价格约定
-const basePrice : number = 0.953;
-let levelPrice : number[] = [basePrice];
-for (let i = 0 ; i < 15 ; i++) {
-	let item  = levelPrice[i] * (1 - 0.03);
+const basePrice: number = 0.953;
+let levelPrice: number[] = [basePrice];
+for (let i = 0; i < 15; i++) {
+	let item = levelPrice[i] * (1 - 0.03);
 	levelPrice.push(Number(item.toFixed(3)));
 }
 // console.info("参考价格：" , levelPrice);
@@ -35,7 +35,7 @@ for (let i = 0 ; i < 15 ; i++) {
  * @return {number}
  */
 function gridGain(grid: Grid) {
-	return ( grid.sellPrice - grid.buyPrice ) * grid.amount;
+	return (grid.sellPrice - grid.buyPrice) * grid.amount;
 }
 
 /**
@@ -43,7 +43,7 @@ function gridGain(grid: Grid) {
  * @param {Grid} grid
  * @return {number} 0: 已进货， 1：已出货
  */
-function gridState(grid: Grid) : number {
+function gridState(grid: Grid): number {
 	if (grid.sellDate === undefined) {
 		return 0;
 	} else {
@@ -56,10 +56,10 @@ function gridState(grid: Grid) : number {
  * @param {Grid} grid
  * @return {number}
  */
-function gridSellPrice(grid: Grid) : number {
-	for (let i = 0 ; i < levelPrice.length ; i++) {
+function gridSellPrice(grid: Grid): number {
+	for (let i = 0; i < levelPrice.length; i++) {
 		if (grid.buyPrice > levelPrice[i]) {
-			return levelPrice[i-2];
+			return levelPrice[i - 2];
 		}
 	}
 	return -1;
@@ -70,8 +70,8 @@ function gridSellPrice(grid: Grid) : number {
  * @param {Grid} grid
  * @return {number}
  */
-function getNextGridPrice(grid: Grid) : number {
-	for (let i = 0 ; i < levelPrice.length ; i++) {
+function getNextGridPrice(grid: Grid): number {
+	for (let i = 0; i < levelPrice.length; i++) {
 		if (grid.buyPrice > levelPrice[i]) {
 			return levelPrice[i];
 		}
@@ -80,33 +80,117 @@ function getNextGridPrice(grid: Grid) : number {
 	// return (grid.buyPrice) * (1 - 0.03);
 }
 
+MongoClient.connect(bdUrl, function (err, db) {
+	if (err) throw err;
+	const dbo = db.db(dbName);
+	let findStr = {'type':1};
+	dbo.collection("grid").find(findStr).toArray(function (err, result) {
+		if (err) throw err;
+		let dealData: Grid[] = [];
+		for (let i = 0; i < result.length; i++) {
+			let item: Grid = {
+				type: result[i].type,
+				amount: result[i].amount,
+				buyPrice: result[i].buyPrice,
+				buyDate: new Date(result[i].buyDate),
+				sellPrice: result[i].sellPrice,
+				sellDate: new Date(result[i].sellDate)
+			};
+			dealData.push(item);
+		};
+		db.close();
+		let minGrid = dealData[0];
+		for(let i = 0 ; i < dealData.length ; i++) {
+			if (dealData[i].buyPrice < minGrid.buyPrice){
+				minGrid = dealData[i];
+			}
+		}
+		console.info('minGrid:',minGrid);
+	});
+});
+
+
+
+
 //todo 2.  录入网格
 
-// 解析 JSON 数据
-let dealData : Grid[] = [];
 
-MongoClient.connect(bdUrl, function(err, db) {
-    if (err) throw err;
-    const dbo = db.db(dbName);
-    var whereStr = {"buyDate": "2018-5-15"}; 
-    let emptyStr = {};
-    dbo.collection("grid"). find(emptyStr).toArray(function(err, result) { 
-        if (err) throw err;
-        for (let i = 0 ; i < result.length ; i++){
-            let item : Grid = {
-                type : result[i].type,
-                amount : result[i].amount,
-                buyPrice : result[i].buyPrice,
-                buyDate : new Date(result[i].buyDate),
-                sellPrice : result[i].sellPrice,
-                sellDate : new Date(result[i].sellDate)
-            };
-            dealData.push(item);
-        }
-        console.info('dealData:',dealData);
-        db.close();
-    });
-});
 
 // 打印所有网格
 // console.info(dealData);
+
+
+let grid = {
+	/** 网格规则 - 价格约定 */
+	getLevelPrice: function () {
+		return levelPrice;
+	},
+	/** 获取所有网格 */
+	getAllGrid: function () {
+		MongoClient.connect(bdUrl, function (err, db) {
+			if (err) throw err;
+			const dbo = db.db(dbName);
+			let emptyStr = {};
+			dbo.collection("grid").find(emptyStr).toArray(function (err, result) {
+				if (err) throw err;
+				let dealData: Grid[] = [];
+				for (let i = 0; i < result.length; i++) {
+					let item: Grid = {
+						type: result[i].type,
+						amount: result[i].amount,
+						buyPrice: result[i].buyPrice,
+						buyDate: new Date(result[i].buyDate),
+						sellPrice: result[i].sellPrice,
+						sellDate: new Date(result[i].sellDate)
+					};
+					dealData.push(item);
+				};
+				db.close();
+				return dealData;
+			});
+		});
+	},
+	/** 当前网格 */
+	getThisGrid: function () {
+		// return minGrid;
+		MongoClient.connect(bdUrl, function (err, db) {
+			if (err) throw err;
+			const dbo = db.db(dbName);
+			let findStr = {'type':1};
+			dbo.collection("grid").find(findStr).toArray(function (err, result) {
+				if (err) throw err;
+				let dealData: Grid[] = [];
+				for (let i = 0; i < result.length; i++) {
+					let item: Grid = {
+						type: result[i].type,
+						amount: result[i].amount,
+						buyPrice: result[i].buyPrice,
+						buyDate: new Date(result[i].buyDate),
+						sellPrice: result[i].sellPrice,
+						sellDate: new Date(result[i].sellDate)
+					};
+					dealData.push(item);
+				};
+				db.close();
+				let minGrid = dealData[0];
+				for(let i = 0 ; i < dealData.length ; i++) {
+					if (dealData[i].buyPrice < minGrid.buyPrice){
+						minGrid = dealData[i];
+					}
+				}
+				return minGrid;
+			});
+		});
+	},
+	/** 出货价格 */
+	getGridSellPrice: function () {
+		// return gridSellPrice(minGrid);
+	},
+	/** 入货价格 */
+	getNextGridPrice: function () {
+		// return getNextGridPrice(minGrid);
+	}
+
+}
+
+export default grid;
--- src/grid-test.js
+++ src/grid-test.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://127.0.0.1:27017/";
+var url = "mongodb://132.232.142.128:27017/";
 
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
